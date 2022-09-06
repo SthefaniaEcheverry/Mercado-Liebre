59774ea83c202f7039cd1d6bbb99349fe34c3ed2
SthefaniaEcheverry <sthefaniaecheverry@gmail.com>
2022-09-05 22:02:09 -0500

    Avances del formulario

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ app.use(express.static('public'));
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
-});
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/register.html"))
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/login.html"))
+})
+
+app.post("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
